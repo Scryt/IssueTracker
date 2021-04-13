eaeb14fd64d59ba75fca7a64c21f944ae399737e
scryt <przemyslaw.tos@gmail.com>
2021-04-13 18:03:38 +0200

    getStatuses has been added - required for add/edit option

---
+++ backend/src/database/Statuses.js
@@ -0,0 +1,26 @@
+const {runSelectQuery, runInsertQuery} = require('./db')
+
+getStatuses = async (status_tag) => {
+    let query = `
+        SELECT S.id,
+               S.tag,
+               D.en AS status_en
+        FROM Status AS S
+                 LEFT JOIN Dictionary D on S.dictionary_id = D.id
+        WHERE is_active = 1
+    `
+
+    const results = await runSelectQuery(query);
+    let resultsObject = {}
+    let tArray = []
+    results.forEach(record => {
+        tArray.push(record)
+    })
+
+    resultsObject["issues"] = tArray;
+    return JSON.stringify(resultsObject);
+};
+
+module.exports = {
+    getStatuses
+}
