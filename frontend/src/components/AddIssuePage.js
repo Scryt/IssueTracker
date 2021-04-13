02524b600b18b94ba124b510e32f37cfdbe92d13
scryt <przemyslaw.tos@gmail.com>
2021-04-13 18:01:22 +0200

    AddIssuePage changed to IssuePage as it will be responsible for both edit and add

--- frontend/src/components/AddIssuePage.js
+++
@@ -1,7 +0,0 @@
-const AddIssuePage = () => (
-    <div>
-        <p> add Issue page</p>
-    </div>
-);
-
-export default AddIssuePage
---
+++ frontend/src/components/IssuePage/IssuePage.js
@@ -0,0 +1,84 @@
+import {useState, useHistory} from "react";
+import {useFetch} from "../../hooks/hooks";
+import style from "../IssueDashboardPage/IssueDashboardPage.module.scss";
+// import style from './IssuePage.module.scss'
+
+const IssuePage = () => {
+    const [data, loading] = useFetch(
+        "http://localhost:8000/statuses"
+    );
+
+    const [inputs, setInputs] = useState({
+        title: '',
+        description: '',
+        status: 'OPEN'
+    });
+
+    const handleInputChange = (event) => {
+        event.persist();
+        setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
+    };
+
+    const handleSubmit = (event) => {
+        const history = useHistory()
+        if (event) {
+            event.preventDefault();
+        }
+
+        fetch('http://localhost:8000/addIssue', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(inputs)
+        })
+
+        history.push("/");
+    };
+
+    return (
+        <div>
+            <form className='register-form' onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="title"
+                    value={inputs.title}
+                    onChange={handleInputChange}
+                    required
+                />
+
+                <input
+                    type="text"
+                    name="description"
+                    value={inputs.description}
+                    onChange={handleInputChange}
+                    required
+                />
+                {loading ? (
+                    "Loading..."
+                ) : (
+                    <select
+                        name="status"
+                        value={inputs.status}
+                        onChange={handleInputChange}
+                    >
+                        {data.issues.map(({id, tag, status_en}) => (
+                            <option
+                                key={id}
+                                value={tag}
+                            >
+                                {status_en}
+                            </option>
+                        ))}
+                    </select>
+                )}
+                <button className="form-field" type="submit">
+                    Add
+                </button>
+            </form>
+
+        </div>
+    );
+};
+
+export default IssuePage
